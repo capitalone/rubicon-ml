c759a716099adfeb2e54d1431cd24393725eff60
Joe Wolfe <joe.wolfe21@gmail.com>
2021-03-01 12:50:40 -0500

    GitHub pages workflow (#15)
    * match conda forge meta
    
    * standard top level comments
    
    * publish docs workflow
    
    * for testing
    
    * full path
    
    * check path
    
    * add the shebang line
    
    * default to bash shell
    
    * set bash on step itself
    
    * cant get github actions to run bash
    
    * switch to on release

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -1,17 +1,17 @@
-if [[ $(pwd) =~ .*/rubicon/docs$ ]]; then
-    if [ "$1" != "--no-clone" ]; then
-        rm -rf build
-        mkdir build
-        cd build
-        git clone -b gh-pages https://github.com/capitalone/rubicon.git html
-        cd ..
-    else
-        rm -rf build/html
-        mkdir build/html
-    fi
-    make html
-    open build/html/index.html
-    echo "Opening $(pwd)/build/html/index.html..."
+#!/bin/bash
+
+echo "Run 'build-docs.sh' from rubicon/docs"
+
+if [ "$1" != "--no-clone" ]; then
+    rm -rf build
+    mkdir build
+    cd build
+    git clone -b gh-pages https://github.com/capitalone/rubicon.git html
+    cd ..
 else
-    echo "'build-docs.sh' must be run from rubicon/docs"
+    rm -rf build/html
+    mkdir build/html
 fi
+make html
+open build/html/index.html
+echo "Opening $(pwd)/build/html/index.html..."
