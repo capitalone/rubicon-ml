0ff326178d43b605e533d3e10d2c19e3a37e50d2
ryanSoley <ryan.e.soley@gmail.com>
2021-03-10 09:47:49 -0500

    remove fsspec bug workarounds

--- rubicon/repository/memory.py
+++ rubicon/repository/memory.py
@@ -1,4 +1,3 @@
-import os
 import pickle
 
 import fsspec
@@ -30,38 +29,6 @@ class MemoryRepository(LocalRepository):
         self.root_dir = root_dir.rstrip("/") if root_dir is not None else "/root"
 
         self.filesystem.mkdir(self.root_dir)
-
-    def _add_dirnames_to_pseudo_dirs(self, path):
-        """Adds each persisted in-memory file's directory to
-        `fsspec`'s `pseudo_dirs`.
-
-        This is a workaround to a bug in `fsspec` where the
-        memory filesystem does not properly populate the
-        `pseudo_dirs` list.
-        """
-        pseudo_dirs = set(self.filesystem.pseudo_dirs)
-
-        while path != "" and path != "/":
-            path = os.path.dirname(path)
-            pseudo_dirs.add(path)
-
-        self.filesystem.pseudo_dirs = list(pseudo_dirs)
-
-    def _persist_bytes(self, bytes_data, path):
-        """Persists the Rubicon object `domain` to the
-        in-memory path defined by `path`.
-        """
-        super()._persist_bytes(bytes_data, path)
-
-        self._add_dirnames_to_pseudo_dirs(path)
-
-    def _persist_domain(self, domain, path):
-        """Persists the Rubicon object `domain` to the
-        in-memory path defined by `path`.
-        """
-        super()._persist_domain(domain, path)
-
-        self._add_dirnames_to_pseudo_dirs(path)
 
     def _persist_dataframe(self, df, path):
         """Persists the `dask` dataframe `df` to the in-memory
