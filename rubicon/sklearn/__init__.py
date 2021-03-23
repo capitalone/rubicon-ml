eef5b1dd710f34d363916eca1a032329f6762c4b
ryanSoley <ryan.e.soley@gmail.com>
2021-03-23 15:13:42 -0400

    base logger

--- rubicon/sklearn/__init__.py
+++ rubicon/sklearn/__init__.py
@@ -1,3 +1,11 @@
+from rubicon.sklearn.base_logger import BaseEstimatorLogger
 from rubicon.sklearn.pipeline import RubiconPipeline
 
-__all__ = ["RubiconPipeline"]
+_logger_mappings = {}
+
+
+def get_logger(sklearn_class_name):
+    return _logger_mappings.get(sklearn_class_name, BaseEstimatorLogger)
+
+
+__all__ = ["get_logger", "RubiconPipeline"]
