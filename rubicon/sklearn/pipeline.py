89b5d9790f1d1bff4bab9a49e6c6df6458d10a0e
Joe Wolfe <joe.wolfe21@gmail.com>
2021-03-24 16:10:34 -0400

    Grid search (#53)
    * add grid search example
    * cleanup for demo
    * ignore unserializable params
    * combine grid search into base example notebook
    * simplify grid search example
    * use tags to filter out empty
    * ignore bad params
    
    Co-authored-by: ryanSoley <ryan.e.soley@gmail.com>

--- rubicon/sklearn/pipeline.py
+++ rubicon/sklearn/pipeline.py
@@ -10,15 +10,19 @@ class RubiconPipeline(Pipeline):
 
         super().__init__(steps, memory=memory, verbose=verbose)
 
-    def fit(self, X, y=None, **fit_params):
+    def fit(self, X, y=None, tags=None, **fit_params):
         pipeline = super().fit(X, y, **fit_params)
 
+        # empty experiments are being logged during
+        # the grid search run so using tags to track
+        # the relevant data
+        if tags is not None:
+            self.experiment.add_tags(tags)
+
         for step_name, estimator in self.steps:
-            print(f"Pipeline step: {step_name}")
             logger_cls = get_logger(estimator.__class__.__name__)
             logger = logger_cls(self.experiment, step_name)
 
-            print("log the relavant params...")
             logger.log(parameters=estimator.get_params())
 
         return pipeline
@@ -26,7 +30,6 @@ class RubiconPipeline(Pipeline):
     def score(self, X, y=None, sample_weight=None):
         score = super().score(X, y, sample_weight)
 
-        print("log the score as a metric...")
-        self.experiment.log_metric("dunno", value=score)
+        self.experiment.log_metric("accuracy", value=score)
 
         return score
