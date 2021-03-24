7c19ce7166c191cc7b43ab9908bc605461109234
Ryan Soley <ryan.e.soley@gmail.com>
2021-03-24 10:01:59 -0400

    try using base logger (#52)
    * try using base logger
    
    * example of custom estimator logger
    
    Co-authored-by: joe-wolfe21 <joe.wolfe21@gmail.com>

--- rubicon/sklearn/__init__.py
+++ rubicon/sklearn/__init__.py
@@ -1,11 +1,4 @@
-from rubicon.sklearn.base_logger import BaseEstimatorLogger
+from rubicon.sklearn.logger_mapping import get_logger
 from rubicon.sklearn.pipeline import RubiconPipeline
-
-_logger_mappings = {}
-
-
-def get_logger(sklearn_class_name):
-    return _logger_mappings.get(sklearn_class_name, BaseEstimatorLogger)
-
 
 __all__ = ["get_logger", "RubiconPipeline"]
