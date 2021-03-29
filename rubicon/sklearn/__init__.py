4164e7cb3a434c6d7bfbf7017c94e1cd1d7e928d
Ryan Soley <ryan.e.soley@gmail.com>
2021-03-29 11:15:22 -0400

    filtered logger (#58)
    * try user defined loggers
    * don't need this anymore
    * change ignore logger to filtered logger
    * instantiate loggers before passing
    * project first
    * clean up names

--- rubicon/sklearn/__init__.py
+++ rubicon/sklearn/__init__.py
@@ -1,4 +1,4 @@
-from rubicon.sklearn.logger_mapping import get_logger
+from rubicon.sklearn.filter_logger import FilterLogger
 from rubicon.sklearn.pipeline import RubiconPipeline
 
-__all__ = ["get_logger", "RubiconPipeline"]
+__all__ = ["FilterLogger", "RubiconPipeline"]
