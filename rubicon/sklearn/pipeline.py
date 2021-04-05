4c7cab903e4fb796e9f7e57add43d43e752d8beb
joe-wolfe21 <joe.wolfe21@gmail.com>
2021-04-05 13:15:40 -0400

    cleanup and add docstrings

--- rubicon/sklearn/pipeline.py
+++ rubicon/sklearn/pipeline.py
@@ -7,7 +7,7 @@ class RubiconPipeline(Pipeline):
     """An extension of `sklearn.pipeline.Pipeline` that automatically
     creates a Rubicon `experiment` under the provided `project` and logs
     the pipeline's `parameters` and `metrics` to it.
-    
+
     A single pipeline run will result in a single `experiment` logged with
     its corresponding `parameters` and `metrics` pulled from the pipeline's
     estimators.
@@ -46,43 +46,61 @@ class RubiconPipeline(Pipeline):
     >>> )
     """
 
-    def __init__(self, project, steps, user_defined_loggers={}, experiment_kwargs={'name': 'RubiconPipeline experiment'}, **kwargs):
+    def __init__(
+        self,
+        project,
+        steps,
+        user_defined_loggers={},
+        experiment_kwargs={"name": "RubiconPipeline experiment"},
+        **kwargs
+    ):
         self.project = project
         self.user_defined_loggers = user_defined_loggers
         self.experiment_kwargs = experiment_kwargs
 
         self.experiment = project.log_experiment(**self.experiment_kwargs)
-        self.logger = BaseLogger()
 
         super().__init__(steps, **kwargs)
 
     def fit(self, X, y=None, tags=None, **fit_params):
+        """Fit the model and automatically log the `fit_params`
+        to Rubicon. Optionally, pass `tags` to update the experiment's
+        tags.
+        """
         pipeline = super().fit(X, y, **fit_params)
 
-        # empty experiments are being logged during
-        # the grid search run so using tags to track
-        # the relevant data
         if tags is not None:
             self.experiment.add_tags(tags)
 
         for step_name, estimator in self.steps:
-            # dynamically update logger if user defined one for this estimator
-            if self.user_defined_loggers.get(step_name):
-                self.set_logger(self.user_defined_loggers.get(step_name))
-
-            self.logger.set_experiment(self.experiment)
-            self.logger.set_step_name(step_name)
-            self.logger.set_estimator(estimator)
-
-            self.logger.log_parameters()
+            logger = self.get_estimator_logger(step_name, estimator)
+            logger.log_parameters()
 
         return pipeline
 
     def score(self, X, y=None, sample_weight=None):
+        """Score with the final estimator and automatically
+        log the results to Rubicon.
+        """
         score = super().score(X, y, sample_weight)
-        self.logger.log_metric("accuracy", score)
+        
+        logger = self.get_estimator_logger()
+        logger.log_metric("score", score)
 
         return score
 
-    def set_logger(self, logger):
-        self.logger = logger
+    def get_estimator_logger(self, step_name=None, estimator=None):
+        """Get a logger for the estimator. By default, the logger will
+        have the current experiment set. 
+        """
+        logger = self.user_defined_loggers.get(step_name) or BaseLogger()
+
+        logger.set_experiment(self.experiment)
+
+        if step_name:
+            logger.set_step_name(step_name)
+        
+        if estimator:
+            logger.set_estimator(estimator)
+        
+        return logger
