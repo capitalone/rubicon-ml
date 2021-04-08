62a2c8b2e8c5a619568bc0798374925a8d69755d
joe-wolfe21 <joe.wolfe21@gmail.com>
2021-04-08 09:35:24 -0400

    cleanup

--- tests/unit/sklearn/test_pipeline.py
+++ tests/unit/sklearn/test_pipeline.py
@@ -2,15 +2,17 @@ from rubicon.sklearn import RubiconPipeline
 from rubicon.sklearn.estimator_logger import EstimatorLogger
 from rubicon.sklearn.filter_estimator_logger import FilterEstimatorLogger
 
+
 def test_get_default_estimator_logger(project_client, fake_estimator_cls):
     project = project_client
     estimator = fake_estimator_cls()
     steps = [("est", estimator)]
     pipeline = RubiconPipeline(project, steps)
-    
+
     logger = pipeline.get_estimator_logger()
 
     assert type(logger) == EstimatorLogger
+
 
 def test_get_user_defined_estimator_logger(project_client, fake_estimator_cls):
     project = project_client
@@ -18,7 +20,7 @@ def test_get_user_defined_estimator_logger(project_client, fake_estimator_cls):
     steps = [("est", estimator)]
     user_defined_logger = {"est": FilterEstimatorLogger(ignore_all=True)}
     pipeline = RubiconPipeline(project, steps, user_defined_logger)
-    
-    logger = pipeline.get_estimator_logger('est')
+
+    logger = pipeline.get_estimator_logger("est")
 
     assert type(logger) == FilterEstimatorLogger
