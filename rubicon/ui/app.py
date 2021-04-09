c7479427cffdb4a45e66aabd1ccf3ede3925cc3e
Ryan Soley <ryan.e.soley@gmail.com>
2021-04-09 08:25:37 -0400

    try out jupyter-dash

--- rubicon/ui/app.py
+++ rubicon/ui/app.py
@@ -1,3 +1,9 @@
 import dash
+from jupyter_dash import JupyterDash
 
 app = dash.Dash(__name__, title="Rubicon")
+
+
+def set_jupyter_app():
+    global app
+    app = JupyterDash(__name__, title="Rubicon")
--- rubicon/ui/dashboard.py
+++ rubicon/ui/dashboard.py
@@ -1,12 +1,7 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
-from rubicon.ui.app import app
 from rubicon.ui.model import RubiconModel
-from rubicon.ui.views.footer import make_footer_layout
-from rubicon.ui.views.header import make_header_layout
-from rubicon.ui.views.project_explorer import make_project_explorer_layout
-from rubicon.ui.views.project_selection import make_project_selection_layout
 
 
 class Dashboard:
@@ -23,17 +18,35 @@ class Dashboard:
     page_size : int, optional
         The number of rows that will be displayed on a page within the
         experiment table.
+    mode : str, optional
+        Where to run the dashboard. Can be one of ["localhost", "jupyterlab"].
+        Defaults to "localhost".
     storage_options : dict, optional
         Additional keyword arguments specific to the protocol being chosen. They
         are passed directly to the underlying filesystem class.
     """
 
-    def __init__(self, persistence, root_dir=None, page_size=10, **storage_options):
+    def __init__(
+        self, persistence, root_dir=None, page_size=10, mode="localhost", **storage_options
+    ):
+        if mode == "jupyterlab":
+            from rubicon.ui.app import set_jupyter_app
+
+            set_jupyter_app()
+
+        # defer importing views until we set the user's desired `app`
+        from rubicon.ui.app import app
+        from rubicon.ui.views.footer import make_footer_layout
+        from rubicon.ui.views.header import make_header_layout
+        from rubicon.ui.views.project_explorer import make_project_explorer_layout
+        from rubicon.ui.views.project_selection import make_project_selection_layout
+
+        self.rubicon_model = RubiconModel(persistence, root_dir, **storage_options)
+        self._mode = mode
+
         self._app = app
         self._app._page_size = page_size
-        self.rubicon_model = RubiconModel(persistence, root_dir, **storage_options)
         self._app._rubicon_model = self.rubicon_model
-
         self._app.layout = html.Div(
             [
                 dbc.Row(make_header_layout()),
@@ -63,4 +76,11 @@ class Dashboard:
         kwargs : dict
             Arguments passed to dash.run_server()
         """
+        if self._mode == "jupyterlab":
+            kwargs.update({"mode": "jupyterlab"})
+
+            # JupyterDash has opposite `debug` default
+            if kwargs.get("debug") is None:
+                kwargs.update({"debug": False})
+
         self._app.run_server(**kwargs)
