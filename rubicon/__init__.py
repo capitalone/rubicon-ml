2b61bb5add42a634ee6f8261fafa0f21a99b575c
ryanSoley <ryan.e.soley@gmail.com>
2021-05-07 10:11:22 -0400

    top level import redirect

--- rubicon/__init__.py
+++ rubicon/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+
 from rubicon.client import (
     Artifact,
     Dataframe,
@@ -13,6 +15,12 @@ from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
+
+rename_warning_message = (
+    "`rubicon` will be renamed to `rubicon_ml` in version 0.3.0. You "
+    "can switch your imports now: `from rubicon_ml import Rubicon`."
+)
+warnings.warn(rename_warning_message)
 
 __all__ = [
     "Artifact",
---
+++ rubicon_ml/__init__.py
@@ -0,0 +1,22 @@
+from rubicon import __version__  # noqa: F401
+from rubicon.client import (
+    Artifact,
+    Dataframe,
+    Experiment,
+    Feature,
+    Metric,
+    Parameter,
+    Project,
+    Rubicon,
+)
+
+__all__ = [
+    "Artifact",
+    "Dataframe",
+    "Experiment",
+    "Feature",
+    "Metric",
+    "Parameter",
+    "Project",
+    "Rubicon",
+]
