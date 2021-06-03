27bf288bf29bcf2739eab8a82a174c4d5e1b8feb
Ryan Soley <ryan.e.soley@gmail.com>
2021-06-03 15:40:08 -0400

    address whitesource vulnerability (#100)
    * manually update underscore.js
    * definitely remove each old underscore.js

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 if [[ $(pwd) =~ .*/rubicon-ml/docs$ ]]; then
-    if [ "$1" != "--no-clone" ]; then
-        rm -rf build
-        mkdir build
-        cd build
-        git clone -b gh-pages https://github.com/capitalone/rubicon-ml.git html
-        cd ..
-    else
-        rm -rf build/html
-        mkdir build/html
-    fi
+    rm -rf build/html
+    mkdir build/html
+
     make html && echo "Open with 'open $(pwd)/build/html/index.html'"
 else
     echo "'build-docs.sh' must be run from rubicon-ml/docs"
