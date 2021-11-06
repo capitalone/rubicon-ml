54400f7cae4d2e9a42d799c66212bc43bfe497b1
Ryan Soley <ryan.e.soley@gmail.com>
2021-11-06 12:26:27 -0400

    custom toggle columns dropdown

--- rubicon_ml/viz/experiments_table.py
+++ rubicon_ml/viz/experiments_table.py
@@ -1,6 +1,6 @@
 import dash_bootstrap_components as dbc
-from dash import dash_table, html
-from dash.dependencies import Input, Output, State
+from dash import dash_table, dcc, html
+from dash.dependencies import ALL, Input, Output, State
 
 from rubicon_ml.viz.base import VizBase
 from rubicon_ml.viz.colors import plot_background_blue
@@ -69,6 +69,72 @@ class ExperimentsTable(VizBase):
         _register_callbacks(self.app)
 
     def _build_layout(self):
+        bulk_select_buttons = [
+            dbc.Button(
+                "select all",
+                id="select-all-button",
+            ),
+            dbc.Button(
+                "clear all",
+                id="clear-all-button",
+            ),
+        ]
+
+        experiment_table = dash_table.DataTable(
+            id="experiment-table",
+            data=self.experiment_records,
+            columns=[
+                {"name": column, "id": column, "selectable": True, "hideable": True}
+                for column in self.all_columns
+            ],
+            hidden_columns=self.hidden_columns,
+            filter_action="native",
+            fixed_columns={"headers": True, "data": 1},
+            page_size=10,
+            row_selectable="multi",
+            sort_action="native",
+            sort_mode="multi",
+            selected_rows=[],
+            style_data_conditional=[
+                {"if": {"row_index": "odd"}, "backgroundColor": plot_background_blue}
+            ],
+            style_header={"fontWeight": 700},
+            style_cell={"overflow": "hidden", "textOverflow": "ellipsis"},
+            style_table={"minWidth": "100%"},
+        )
+
+        toggle_columns_dropdown = dbc.DropdownMenu(
+            [
+                dbc.DropdownMenuItem(
+                    "show all",
+                    id="show-all-dropdown-button",
+                ),
+                dbc.DropdownMenuItem(
+                    "hide all",
+                    id="hide-all-dropdown-button",
+                ),
+                dbc.DropdownMenuItem(divider=True),
+                *[
+                    dbc.DropdownMenuItem(
+                        dcc.Checklist(
+                            inputClassName="column-dropdown-checkbox",
+                            labelClassName="column-dropdown-label",
+                            options=[{"label": column, "value": column}],
+                            value=[column] if column not in self.hidden_columns else [],
+                            id={
+                                "type": "column-dropdown-checkbox",
+                                "index": column,
+                            },
+                        ),
+                        id={"type": "column-dropdown-button", "index": column},
+                    )
+                    for column in self.all_columns
+                ],
+            ],
+            id="column-selection-dropdown",
+            label="toggle columns",
+        )
+
         return html.Div(
             [
                 self._to_store(ignore_attributes=["app", "experiments"]),
@@ -78,55 +144,65 @@ class ExperimentsTable(VizBase):
                     ),
                     className="header-row",
                 ),
-                html.Div(
+                dbc.Row(
+                    [
+                        *[dbc.Col(button) for button in bulk_select_buttons],
+                        dbc.Col(toggle_columns_dropdown),
+                    ],
                     className="button-group",
-                    children=[
-                        dbc.Button(
-                            "select all",
-                            id="select-all-button",
-                        ),
-                        dbc.Button(
-                            "clear all",
-                            id="clear-all-button",
-                        ),
-                        dbc.Button(
-                            "show all columns",
-                            id="show-all-columns-button",
-                        ),
-                        dbc.Button(
-                            "hide all columns",
-                            id="hide-all-columns-button",
-                        ),
-                    ],
                 ),
-                dash_table.DataTable(
-                    id="experiment-table",
-                    data=self.experiment_records,
-                    columns=[
-                        {"name": column, "id": column, "selectable": True, "hideable": True}
-                        for column in self.all_columns
-                    ],
-                    hidden_columns=self.hidden_columns,
-                    filter_action="native",
-                    fixed_columns={"headers": True, "data": 1},
-                    page_size=10,
-                    row_selectable="multi",
-                    sort_action="native",
-                    sort_mode="multi",
-                    selected_rows=[],
-                    style_data_conditional=[
-                        {"if": {"row_index": "odd"}, "backgroundColor": plot_background_blue}
-                    ],
-                    style_header={"fontWeight": 700},
-                    style_cell={"overflow": "hidden", "textOverflow": "ellipsis"},
-                    style_table={"minWidth": "100%"},
-                ),
+                experiment_table,
             ],
             style={"width": "100%"},
         )
 
 
 def _register_callbacks(app):
+    @app.callback(
+        Output({"type": "column-dropdown-checkbox", "index": ALL}, "value"),
+        [
+            Input("show-all-dropdown-button", "n_clicks_timestamp"),
+            Input("hide-all-dropdown-button", "n_clicks_timestamp"),
+        ],
+        State("memory-store", "data"),
+        prevent_initial_call=True,
+    )
+    def _update_selected_column_checkboxes(last_show_click, last_hide_click, data):
+        """Bulk updates for the selections in the "toggle columns" dropdown.
+
+        Returns all if triggered by the "show all" button and returns
+        only "id" if triggered by the "hide all" button. The initial
+        call is prevented as the default state is neither all nor none.
+        """
+        last_show_click = last_show_click if last_show_click else 0
+        last_hide_click = last_hide_click if last_hide_click else 0
+
+        all_columns = data["all_columns"]
+
+        if last_hide_click > last_show_click:
+            hidden_values = [[]] * (len(all_columns) - 1)
+
+            return [["id"], *hidden_values]
+
+        return [[column] for column in all_columns]
+
+    @app.callback(
+        Output("experiment-table", "hidden_columns"),
+        Input({"type": "column-dropdown-checkbox", "index": ALL}, "value"),
+        State("memory-store", "data"),
+    )
+    def _update_hidden_experiment_table_cols(selected_columns, data):
+        """Hide and show the columns in the experiment table.
+
+        Returns the columns that should be hidden based on whether or
+        not the column's corresponding value is checked in the "toggle
+        columns" dropdown.
+        """
+        all_columns = data["all_columns"]
+        selected_columns = [sc[0] for sc in selected_columns if len(sc) > 0]
+
+        return [column for column in all_columns if column not in selected_columns]
+
     @app.callback(
         Output("experiment-table", "selected_rows"),
         [
@@ -138,34 +214,15 @@ def _register_callbacks(app):
     def _update_selected_experiment_table_rows(
         last_select_click, last_clear_click, experiment_table_indices
     ):
+        """Bulk selection for the rows of the experiment table.
+
+        Returns all if triggered by the "select all" button and returns
+        none if triggered by the "clear all" button.
+        """
         last_select_click = last_select_click if last_select_click else 0
         last_clear_click = last_clear_click if last_clear_click else 0
 
-        if int(last_select_click) > int(last_clear_click):
+        if last_select_click > last_clear_click:
             return experiment_table_indices
 
         return []
-
-    @app.callback(
-        Output("experiment-table", "hidden_columns"),
-        [
-            Input("show-all-columns-button", "n_clicks_timestamp"),
-            Input("hide-all-columns-button", "n_clicks_timestamp"),
-        ],
-        State("memory-store", "data"),
-    )
-    def _update_selected_experiment_table_cols(
-        last_show_click,
-        last_hide_click,
-        data,
-    ):
-        last_show_click = last_show_click if last_show_click else 0
-        last_hide_click = last_hide_click if last_hide_click else 0
-
-        all_columns = data["all_columns"]
-        hidden_columns = data["hidden_columns"]
-
-        if int(last_show_click) >= int(last_hide_click):
-            return hidden_columns
-
-        return [column for column in all_columns if column != "id"]
