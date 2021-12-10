648433af65359f599ed098078a323f60e1184f83
Ryan Soley <ryan.e.soley@gmail.com>
2021-12-10 13:42:23 -0500

    remove artifact plot for now

--- rubicon_ml/viz/__init__.py
+++ rubicon_ml/viz/__init__.py
@@ -1,4 +1,3 @@
-from rubicon_ml.viz.artifact_plot import plot_artifacts
 from rubicon_ml.viz.dataframe_plot import plot_dataframes
 from rubicon_ml.viz.experiments_table import view_experiments_table
 from rubicon_ml.viz.metric_correlation_plot import plot_metric_correlation
@@ -6,7 +5,6 @@ from rubicon_ml.viz.metric_lists_comparison import compare_metric_lists
 
 __all__ = [
     "compare_metric_lists",
-    "plot_artifacts",
     "plot_dataframes",
     "plot_metric_correlation",
     "view_experiments_table",
