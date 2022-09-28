8512000b9b6032b538c4882da16204166887f6af
Armaan Dhillon <dhillon.armaan11@gmail.com>
2022-09-28 09:56:42 -0400

    resolving merge conflict

--- rubicon_ml/repository/base.py
+++ rubicon_ml/repository/base.py
@@ -922,11 +922,8 @@ class BaseRepository:
             "Dataframe": self._get_dataframe_metadata_root,
             "Experiment": self._get_experiment_metadata_root,
             "Metric": self._get_metric_metadata_root,
-<<<<<<< HEAD
             "Feature": self._get_feature_metadata_root,
-=======
             "Parameter": self._get_parameter_metadata_root,
->>>>>>> 31aedcf (updated code to implement parameter tagging)
         }
 
         try:
