63b47673d5444507b8a6dc2ad51e90b6aec1d8ef
Ryan Soley <ryan.e.soley@gmail.com>
2022-11-15 09:27:22 -0500

    traceback formatting passthru

--- rubicon_ml/client/utils/exception_handling.py
+++ rubicon_ml/client/utils/exception_handling.py
@@ -1,17 +1,24 @@
 import logging
+import traceback
 import warnings
 
 FAILURE_MODE = "raise"
 FAILURE_MODES = ["log", "raise", "warn"]
+TRACEBACK_CHAIN = False
+TRACEBACK_LIMIT = None
 
 
-def set_failure_mode(failure_mode):
+def set_failure_mode(failure_mode, traceback_chain=False, traceback_limit=None):
     global FAILURE_MODE
+    global TRACEBACK_CHAIN
+    global TRACEBACK_LIMIT
 
     if failure_mode not in FAILURE_MODES:
         raise ValueError(f"`failure_mode` must be one of {FAILURE_MODES}")
 
     FAILURE_MODE = failure_mode
+    TRACEBACK_CHAIN = traceback_chain
+    TRACEBACK_LIMIT = traceback_limit
 
 
 def failsafe(func):
@@ -22,8 +29,8 @@ def failsafe(func):
             if FAILURE_MODE == "raise":
                 raise e
             elif FAILURE_MODE == "warn":
-                warnings.warn(repr(e))
+                warnings.warn(traceback.format_exc(limit=TRACEBACK_LIMIT, chain=TRACEBACK_CHAIN))
             elif FAILURE_MODE == "log":
-                logging.error(repr(e))
+                logging.error(traceback.format_exc(limit=TRACEBACK_LIMIT, chain=TRACEBACK_CHAIN))
 
     return wrapper
