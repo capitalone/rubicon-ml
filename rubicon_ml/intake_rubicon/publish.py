bb3910779b21c6548651f716d8663d5c1087baf7
Tejas Sameera <sameera.tejas5@gmail.com>
2022-11-28 17:27:51 -0500

    added functionality to update intake catalogs to a given catalog file via function call

--- rubicon_ml/intake_rubicon/publish.py
+++ rubicon_ml/intake_rubicon/publish.py
@@ -5,6 +5,7 @@ import yaml
 def publish(
     experiments,
     output_filepath=None,
+    catalog_filepath=None,
 ):
     """Publish experiments to an `intake` catalog that can be
     read by the `intake-rubicon` driver.
@@ -25,6 +26,9 @@ def publish(
         The YAML string representation of the `intake` catalog
         containing the experiments `experiments`.
     """
+
+    if catalog_filepath is not None:
+        return update_catalog(filepath=catalog_filepath, new_experiments=experiments)
     catalog = {"sources": {}}
 
     for experiment in experiments:
@@ -47,3 +51,30 @@ def publish(
             f.write(catalog_yaml)
 
     return catalog_yaml
+
+
+def update_catalog(filepath, new_experiments):
+
+    updated_catalog = {}
+    for experiment in new_experiments:
+        appended_experiment_catalog = {
+            "driver": "rubicon_ml_experiment",
+            "args": {
+                "experiment_id": experiment.id,
+                "project_name": experiment.project.name,
+                "urlpath": experiment.repository.root_dir,
+            },
+        }
+
+        experiment_catalog_name = f"experiment_{experiment.id.replace('-', '_')}"
+        updated_catalog[experiment_catalog_name] = appended_experiment_catalog
+
+    with open(filepath, "r") as yamlfile:
+        curr_catalog = yaml.safe_load(yamlfile)
+        curr_catalog["sources"].update(updated_catalog)
+
+    if curr_catalog:
+        with open(filepath, "w") as yamlfile:
+            yaml.safe_dump(curr_catalog, yamlfile)
+
+    return curr_catalog
