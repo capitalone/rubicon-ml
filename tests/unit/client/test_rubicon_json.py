0b03debf2db373fdf7fbaa0867619bc1ab8d000f
Tejas Sameera <56408709+Tejass9922@users.noreply.github.com>
2022-12-12 13:11:01 -0500

    added rubicon json to library with skeleton test code (#322)

---
+++ tests/unit/client/test_rubicon_json.py
@@ -0,0 +1,30 @@
+# import os
+# from unittest.mock import MagicMock, patch
+
+# from jsonpath_ng.ext import parse
+
+# from rubicon_ml import domain
+# from rubicon_ml.client import RubiconJSON
+
+
+def test_rubicon_to_json(rubicon_entities):
+    # rubicon_json = RubiconJSON(rubicon_entities)
+    # json_structure = rubicon_json.project_to_json()
+    assert 1 == 1
+
+
+def test_experiment_to_json(experiment):
+
+    assert 1 == 1
+
+
+def test_project_to_json(project):
+
+    assert 1 == 1
+
+
+# Test for helper method too??
+
+
+def test_convert_to_json(rubicon_objects=None, projects=None, experiments=None):
+    assert 1 == 1
