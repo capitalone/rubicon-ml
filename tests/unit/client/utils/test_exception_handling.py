a0dd178f0e9cffc48418d1176ed8ca8341ddc79c
Armaan Dhillon <dhillon.armaan11@gmail.com>
2023-06-26 15:26:48 -0400

    added multiple backend exception testing + raised test coverage

--- tests/unit/client/utils/test_exception_handling.py
+++ tests/unit/client/utils/test_exception_handling.py
@@ -46,7 +46,7 @@ def test_failure_mode_raise(rubicon_client):
     with pytest.raises(RubiconException) as e:
         rubicon_client.get_project(name="does not exist")
 
-    assert "No project with name 'does not exist' found." in repr(e)
+    assert "all configured storage backends failed" in repr(e)
 
 
 @patch("warnings.warn")
