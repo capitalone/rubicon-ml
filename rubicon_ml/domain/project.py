1df0df2ac251d21791d2c009b67be4b17d0d17fc
sonaalthaker <sonaal.r.thaker@gmail.com>
2024-03-06 11:14:39 -0800

    projects cleanup

--- rubicon_ml/domain/project.py
+++ rubicon_ml/domain/project.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import List, Optional
+from typing import Optional
 
-from rubicon_ml.domain.mixin import TagMixin  # noqa F401
 from rubicon_ml.domain.utils import TrainingMetadata, uuid
 
 
@@ -17,4 +16,3 @@ class Project:
     github_url: Optional[str] = None
     training_metadata: Optional[TrainingMetadata] = None
     created_at: datetime = field(default_factory=datetime.utcnow)
-    tags: List[str] = field(default_factory=list)
