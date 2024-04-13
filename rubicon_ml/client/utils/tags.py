950b3a9833877dcda376736b1b4d65528ef42972
Ryan Soley <ryan.e.soley@gmail.com>
2024-04-12 22:08:12 -0400

    simplify `has_tag_requirements`

--- rubicon_ml/client/utils/tags.py
+++ rubicon_ml/client/utils/tags.py
@@ -32,17 +32,9 @@ def has_tag_requirements(tags: List[str], required_tags: List[str], qtype: str) 
     """Returns True if `tags` meets the requirements based on
     the values of `required_tags` and `qtype`. False otherwise.
     """
-    has_tag_requirements = False
+    qtype_func = any if qtype == "or" else all
 
-    tag_intersection = set(required_tags).intersection(set(tags))
-    if qtype == "or":
-        if len(tag_intersection) > 0:
-            has_tag_requirements = True
-    if qtype == "and":
-        if len(tag_intersection) == len(required_tags):
-            has_tag_requirements = True
-
-    return has_tag_requirements
+    return qtype_func(tag in tags for tag in required_tags)
 
 
 def filter_children(children, tags, qtype, name):
