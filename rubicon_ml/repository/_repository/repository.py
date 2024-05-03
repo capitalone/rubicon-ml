c9a1ecd5ba232239e0eaaef95fc230967c778e75
Ryan Soley <ryan.e.soley@gmail.com>
2024-05-02 20:57:28 -0400

    new repository stubs

---
+++ rubicon_ml/repository/_repository/repository.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+
+class RepositoryABC(ABC):
+    """Abstract base for rubicon-ml backend repositories."""
+
+    @abstractmethod
+    def write_json(self):
+        """"""
+        ...
+
+    @abstractmethod
+    def read_json(self):
+        """"""
+        ...
+
+    @abstractmethod
+    def write_bytes(self):
+        """"""
+        ...
+
+    @abstractmethod
+    def read_bytes(self):
+        """"""
+        ...
+
+    @abstractmethod
+    def write_dataframe(self):
+        """"""
+        ...
+
+    @abstractmethod
+    def read_dataframe(self):
+        """"""
+        ...
