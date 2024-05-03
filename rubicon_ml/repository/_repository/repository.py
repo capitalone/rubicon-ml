71a37f387955fba98e9b9237def9bae2273f11bc
Ryan Soley <ryan.e.soley@gmail.com>
2024-05-03 12:39:56 -0400

    path -> location

--- rubicon_ml/repository/_repository/repository.py
+++ rubicon_ml/repository/_repository/repository.py
@@ -17,143 +17,143 @@ class RepositoryABC(ABC):
     """Abstract base for rubicon-ml backend repositories."""
 
     @abstractmethod
-    def _get_artifact_data_path(self, *args) -> str:
+    def _get_artifact_data_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_artifact_metadata_path(self, *args) -> str:
+    def _get_artifact_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_comment_metadata_path(self, *args) -> str:
+    def _get_comment_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_dataframe_data_path(self, *args) -> str:
+    def _get_dataframe_data_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_dataframe_metadata_path(self, *args) -> str:
+    def _get_dataframe_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_experiment_metadata_path(self, *args) -> str:
+    def _get_experiment_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_feature_metadata_path(self, *args) -> str:
+    def _get_feature_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_metric_metadata_path(self, *args) -> str:
+    def _get_metric_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_parameter_metadata_path(self, *args) -> str:
+    def _get_parameter_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_project_metadata_path(self, *args) -> str:
+    def _get_project_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _get_tag_metadata_path(self, *args) -> str:
+    def _get_tag_metadata_location(self, *args) -> str:
         """"""
         ...
 
     @abstractmethod
-    def _read_bytes(self, path: str, *args) -> bytes:
+    def _read_bytes(self, location: str, *args) -> bytes:
         """"""
         ...
 
     @abstractmethod
-    def _read_dataframe(self, path: str, *args) -> "DATAFRAME_TYPES":
+    def _read_dataframe(self, location: str, *args) -> "DATAFRAME_TYPES":
         """"""
         ...
 
     @abstractmethod
-    def _read_domain(self, path: str, *args) -> "DOMAIN_TYPES":
+    def _read_domain(self, location: str, *args) -> "DOMAIN_TYPES":
         """"""
         ...
 
     @abstractmethod
-    def _write_bytes(self, data: bytes, path: str, *args):
+    def _write_bytes(self, data: bytes, location: str, *args):
         """"""
         ...
 
     @abstractmethod
-    def _write_dataframe(self, data: "DATAFRAME_TYPES", path: str, *args):
+    def _write_dataframe(self, data: "DATAFRAME_TYPES", location: str, *args):
         """"""
         ...
 
     @abstractmethod
-    def _write_domain(self, data: "DOMAIN_TYPES", path: str, *args):
+    def _write_domain(self, data: "DOMAIN_TYPES", location: str, *args):
         """"""
         ...
 
-    def _get_path(self, data: Union[bytes, "DOMAIN_TYPES", "DATAFRAME_TYPES"], *args) -> str:
+    def _get_location(self, data: Union[bytes, "DOMAIN_TYPES", "DATAFRAME_TYPES"], *args) -> str:
         """TODO: HANDLE TAGS AND COMMENTS"""
         if isinstance(data, Artifact):
-            return self._get_artifact_metadata_path(*args)
+            return self._get_artifact_metadata_location(*args)
         elif isinstance(data, Dataframe):
-            return self._get_dataframe_metadata_path(*args)
+            return self._get_dataframe_metadata_location(*args)
         elif isinstance(data, Experiment):
-            return self._get_experiment_metadata_path(*args)
+            return self._get_experiment_metadata_location(*args)
         elif isinstance(data, Feature):
-            return self._get_feature_metadata_path(*args)
+            return self._get_feature_metadata_location(*args)
         elif isinstance(data, Metric):
-            return self._get_metric_metadata_path(*args)
+            return self._get_metric_metadata_location(*args)
         elif isinstance(data, Parameter):
-            return self._get_parameter_metadata_path(*args)
+            return self._get_parameter_metadata_location(*args)
         elif isinstance(data, Project):
-            return self._get_project_metadata_path(*args)
+            return self._get_project_metadata_location(*args)
         elif isinstance(data, bytes):
-            return self._get_artifact_data_path(*args)
+            return self._get_artifact_data_location(*args)
         else:
-            return self._get_dataframe_data_path(*args)
+            return self._get_dataframe_data_location(*args)
 
     def read_bytes(self, *args) -> bytes:
         """"""
-        path = self._get_path(*args)
+        location = self._get_location(*args)
 
-        return self._read_bytes(path, *args)
+        return self._read_bytes(location, *args)
 
     def read_dataframe(self, *args) -> "DATAFRAME_TYPES":
         """"""
-        path = self._get_path(*args)
+        location = self._get_location(*args)
 
-        return self._read_dataframe(path, *args)
+        return self._read_dataframe(location, *args)
 
     def read_domain(self, *args) -> "DOMAIN_TYPES":
         """"""
-        path = self._get_path(*args)
+        location = self._get_location(*args)
 
-        return self._read_domain(path, *args)
+        return self._read_domain(location, *args)
 
     def write_bytes(self, data: bytes, *args):
         """"""
-        path = self._get_path(*args)
+        location = self._get_location(*args)
 
-        self._write_bytes(data, path, *args)
+        self._write_bytes(data, location, *args)
 
     def write_dataframe(self, data: "DATAFRAME_TYPES", *args):
         """"""
-        path = self._get_path(*args)
+        location = self._get_location(*args)
 
-        self._write_dataframe(data, path, *args)
+        self._write_dataframe(data, location, *args)
 
     def write_domain(self, data: "DOMAIN_TYPES", *args):
         """"""
-        path = self._get_path(*args)
+        location = self._get_location(*args)
 
-        self._write_domain(data, path, *args)
+        self._write_domain(data, location, *args)
