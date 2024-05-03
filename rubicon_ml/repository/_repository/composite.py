10aa35a981d1e1e02b67a697bb88ec533d652f60
Ryan Soley <ryan.e.soley@gmail.com>
2024-05-03 12:33:25 -0400

    add abstract path getters

--- rubicon_ml/repository/_repository/composite.py
+++ rubicon_ml/repository/_repository/composite.py
@@ -1,10 +1,20 @@
-from typing import TYPE_CHECKING, Dict, List
+from typing import TYPE_CHECKING, Any, Callable, List
 
 from rubicon_ml.exceptions import RubiconException
 from rubicon_ml.repository._repository.repository import RepositoryABC
 
 if TYPE_CHECKING:
-    from rubicon_ml.types import DATAFRAME_TYPES
+    from rubicon_ml.types import DATAFRAME_TYPES, DOMAIN_TYPES
+
+
+def _safe_call_func(func: Callable, *args) -> Any:
+    """"""
+    try:
+        data = func(*args)
+    except Exception:
+        pass
+    else:
+        return data
 
 
 class CompositeRepository(RepositoryABC):
@@ -14,39 +24,24 @@ class CompositeRepository(RepositoryABC):
         """"""
         self.repositories = repositories
 
-    def read_bytes(self) -> bytes:
+    def read_bytes(self, *args) -> bytes:
         """"""
         for repository in self.repositories:
-            try:
-                data = repository.read_bytes()
-            except Exception:
-                pass
-            else:
-                return data
+            return _safe_call_func(repository.read_bytes, *args)
 
         raise RubiconException("All backends failed.")
 
-    def read_dataframe(self) -> "DATAFRAME_TYPES":
+    def read_dataframe(self, *args) -> "DATAFRAME_TYPES":
         """"""
         for repository in self.repositories:
-            try:
-                data = repository.read_dataframe()
-            except Exception:
-                pass
-            else:
-                return data
+            return _safe_call_func(repository.read_dataframe, *args)
 
         raise RubiconException("All backends failed.")
 
-    def read_json(self) -> Dict:
+    def read_domain(self, *args) -> "DOMAIN_TYPES":
         """"""
         for repository in self.repositories:
-            try:
-                data = repository.read_json()
-            except Exception:
-                pass
-            else:
-                return data
+            return _safe_call_func(repository.read_domain, *args)
 
         raise RubiconException("All backends failed.")
 
@@ -60,7 +55,7 @@ class CompositeRepository(RepositoryABC):
         for repository in self.repositories:
             repository.write_dataframe(data, *args)
 
-    def write_json(self, data: Dict, *args):
+    def write_domain(self, data: "DOMAIN_TYPES", *args):
         """"""
         for repository in self.repositories:
-            repository.write_json(data, *args)
+            repository.write_domain(data, *args)
--- rubicon_ml/types.py
+++ rubicon_ml/types.py
@@ -1,11 +1,21 @@
-from typing import TYPE_CHECKING, Union
-
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from typing import Union
+
     import dask.dataframe as dd
     import pandas as pd
 
+    from rubicon_ml.domain.artifact import Artifact
+    from rubicon_ml.domain.dataframe import Dataframe
+    from rubicon_ml.domain.experiment import Experiment
+    from rubicon_ml.domain.feature import Feature
+    from rubicon_ml.domain.metric import Metric
+    from rubicon_ml.domain.parameter import Parameter
+    from rubicon_ml.domain.project import Project
+
     DATAFRAME_TYPES = Union[dd.DataFrame, pd.DataFrame]
+    DOMAIN_TYPES = Union[Artifact, Dataframe, Experiment, Feature, Metric, Parameter, Project]
 
 
 def safe_is_dask_dataframe(dataframe):
