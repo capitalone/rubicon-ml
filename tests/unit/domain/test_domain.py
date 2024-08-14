cff5a286011c10d0c21fb8607054b6cadf1a2d2c
Ryan Soley <ryan.e.soley@gmail.com>
2024-08-14 14:15:15 -0400

    project dataclass - warn on extra kwargs (#474)
    * override domain project init
    * add domain extra kwarg test
    * parametrize test and test warning
    * clean up

---
+++ tests/unit/domain/test_domain.py
@@ -0,0 +1,24 @@
+from unittest import mock
+
+import pytest
+
+from rubicon_ml.domain.project import Project
+
+
+@pytest.mark.parametrize(
+    ["domain_cls", "required_kwargs"],
+    [(Project, {"name": "test_domain_extra_kwargs"})],
+)
+def test_domain_extra_kwargs(domain_cls, required_kwargs):
+    with mock.patch(
+        f"rubicon_ml.domain.{domain_cls.__name__.lower()}.LOGGER.warning"
+    ) as mock_logger_warning:
+        domain = domain_cls(extra="extra", **required_kwargs)
+
+    mock_logger_warning.assert_called_once_with(
+        f"{domain_cls.__name__}.__init__() got an unexpected keyword argument(s): `extra`",
+    )
+
+    assert "extra" not in domain.__dict__
+    for key, value in required_kwargs.items():
+        assert getattr(domain, key) == value
