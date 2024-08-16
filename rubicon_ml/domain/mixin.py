c90968a0a229d2a0993eaca5a7376147fc3d91ef
Ryan Soley <ryan.e.soley@gmail.com>
2024-08-16 15:38:41 -0400

    experiment dataclass - warn on extra kwargs (#475)
    * move init helpers to mixin
    * update experiment domain init
    * fix types

--- rubicon_ml/domain/mixin.py
+++ rubicon_ml/domain/mixin.py
@@ -1,30 +1,9 @@
-from typing import List
+import datetime
+import logging
+import uuid
+from typing import Dict, List, Optional
 
-
-class TagMixin:
-    """Adds tagging support to a domain model."""
-
-    def add_tags(self, tags: List[str]):
-        """
-        Add new tags to this model.
-
-        Parameters
-        ----------
-        tags : List[str]
-            A list of string tags to add to the domain model.
-        """
-        self.tags = list(set(self.tags).union(set(tags)))
-
-    def remove_tags(self, tags: List[str]):
-        """
-        Remove tags from this model.
-
-        Parameters
-        ----------
-        tags : List[str]
-            A list of string tags to remove from this domain model.
-        """
-        self.tags = list(set(self.tags).difference(set(tags)))
+LOGGER = logging.getLogger()
 
 
 class CommentMixin:
@@ -51,3 +30,64 @@ class CommentMixin:
             A list of string comments to remove from this domain model.
         """
         self.comments = list(set(self.comments).difference(set(comments)))
+
+
+class InitMixin:
+    """Adds helpers for initializing default values."""
+
+    def _check_extra_kwargs(self, kwargs: Dict):
+        """Warn if extra kwargs are given when initialized.
+
+        Replaces default `dataclass` behavior of erroring on unexpected kwargs.
+        """
+        if kwargs:
+            LOGGER.warning(
+                f"{self.__class__.__name__}.__init__() got an unexpected keyword "
+                f"argument(s): `{'`, `'.join([key for key in kwargs])}`"
+            )
+
+    def _init_created_at(self, created_at: Optional[datetime.datetime]) -> datetime.datetime:
+        """Initialize the `created_at` attribute to the current time if necessary.
+
+        `datetime.UTC` was added and `datetime.utcnow` was deprecated in Python 3.11.
+        """
+        if created_at is None:
+            try:
+                return datetime.datetime.now(datetime.UTC)
+            except AttributeError:
+                return datetime.datetime.utcnow()
+
+        return created_at
+
+    def _init_id(self, id: Optional[str]) -> str:
+        """Initialize the `id` attribute to a new UUID if necessary."""
+        if id is None:
+            return str(uuid.uuid4())
+
+        return id
+
+
+class TagMixin:
+    """Adds tagging support to a domain model."""
+
+    def add_tags(self, tags: List[str]):
+        """
+        Add new tags to this model.
+
+        Parameters
+        ----------
+        tags : List[str]
+            A list of string tags to add to the domain model.
+        """
+        self.tags = list(set(self.tags).union(set(tags)))
+
+    def remove_tags(self, tags: List[str]):
+        """
+        Remove tags from this model.
+
+        Parameters
+        ----------
+        tags : List[str]
+            A list of string tags to remove from this domain model.
+        """
+        self.tags = list(set(self.tags).difference(set(tags)))
--- tests/unit/domain/test_domain.py
+++ tests/unit/domain/test_domain.py
@@ -2,17 +2,18 @@ from unittest import mock
 
 import pytest
 
-from rubicon_ml.domain.project import Project
+from rubicon_ml.domain import Experiment, Project
 
 
 @pytest.mark.parametrize(
     ["domain_cls", "required_kwargs"],
-    [(Project, {"name": "test_domain_extra_kwargs"})],
+    [
+        (Project, {"name": "test_domain_extra_kwargs"}),
+        (Experiment, {"project_name": "test_domain_extra_kwargs"}),
+    ],
 )
 def test_domain_extra_kwargs(domain_cls, required_kwargs):
-    with mock.patch(
-        f"rubicon_ml.domain.{domain_cls.__name__.lower()}.LOGGER.warning"
-    ) as mock_logger_warning:
+    with mock.patch("rubicon_ml.domain.mixin.LOGGER.warning") as mock_logger_warning:
         domain = domain_cls(extra="extra", **required_kwargs)
 
     mock_logger_warning.assert_called_once_with(
