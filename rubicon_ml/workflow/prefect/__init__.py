a5ede1e228407d799abbade87a514d165d185c7f
Ryan Soley <ryan.e.soley@gmail.com>
2024-09-04 10:17:08 -0400

    mention example in deprecation message

--- rubicon_ml/workflow/prefect/__init__.py
+++ rubicon_ml/workflow/prefect/__init__.py
@@ -13,7 +13,8 @@ def _check_for_prefect_extras():
 
 warnings.warn(
     "The `rubicon_ml.workflow.prefect` module is deprecated and will be removed in an upcoming release."
-    "`rubicon_ml` can still be leveraged within custom tasks.",
+    "`rubicon_ml` can still be leveraged within custom tasks. "
+    "(see https://capitalone.github.io/rubicon-ml/integrations/integration-prefect-workflows.html).",
     DeprecationWarning,
 )
 
--- rubicon_ml/workflow/prefect/tasks.py
+++ rubicon_ml/workflow/prefect/tasks.py
@@ -6,7 +6,8 @@ from rubicon_ml import Rubicon
 
 warnings.warn(
     "The `rubicon_ml.workflow.prefect.tasks` module is deprecated and will be removed in an upcoming release."
-    "`rubicon_ml` can still be leveraged within custom tasks.",
+    "`rubicon_ml` can still be leveraged within custom tasks. "
+    "(see https://capitalone.github.io/rubicon-ml/integrations/integration-prefect-workflows.html).",
     DeprecationWarning,
 )
 
