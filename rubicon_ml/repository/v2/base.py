5a1a859612c30f357d721ea238f686d1b18118fb
Ryan Soley <ryan.e.soley@gmail.com>
2025-04-10 19:47:04 -0400

    feat: start adding methods to repos

--- rubicon_ml/repository/v2/base.py
+++ rubicon_ml/repository/v2/base.py
@@ -1,11 +1,70 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class BaseRepository(ABC):
     """Abstract base for rubicon-ml backend repositories."""
 
-    def __init__(*args, **kwargs):
-        pass
+    # core read/writes
+
+    @abstractmethod
+    def read_domain(self, *args, **kwargs):
+        ...
+
+    @abstractmethod
+    def read_domains(self, *args, **kwargs):
+        ...
+
+    @abstractmethod
+    def write_domain(self, *args, **kwargs):
+        ...
+
+    # binary read/writes
+
+    @abstractmethod
+    def read_artifact_data(self, *args, **kwargs):
+        ...
+
+    @abstractmethod
+    def write_artifact_data(self, *args, **kwargs):
+        ...
+
+    @abstractmethod
+    def read_dataframe_data(self, *args, **kwargs):
+        ...
+
+    @abstractmethod
+    def write_dataframe_data(self, *args, **kwargs):
+        ...
+
+    # domain read/writes
+
+    def read_project_metadata(self, *args, **kwargs):
+        project = self.read_domain(*args, **kwargs)
+
+        return project
+
+    def read_projects_metadata(self, *args, **kwargs):
+        projects = self.read_domains(*args, **kwargs)
+
+        return projects
+
+    def write_project_metadata(self, *args, **kwargs):
+        self.write_domain(*args, **kwargs)
+
+    def read_experiment_metadata(self, *args, **kwargs):
+        experiment = self.read_domain(*args, **kwargs)
+
+        return experiment
+
+    def read_experiments_metadata(self, *args, **kwargs):
+        experiments = self.read_domains(*args, **kwargs)
+
+        return experiments
+
+    def write_experiment_metadata(self, *args, **kwargs):
+        self.write_domain(*args, **kwargs)
+
+    # TODO: features, metrics, parameters, artifacts, dataframes
 
 
 class BaseRepositoryV2(BaseRepository):
--- rubicon_ml/repository/v2/fsspec.py
+++ rubicon_ml/repository/v2/fsspec.py
@@ -1,28 +1,63 @@
+from abc import abstractmethod
+
 from rubicon_ml.repository.v2.base import BaseRepository
 
 
 class FsspecRepository(BaseRepository):
     """Base repository for `fsspec`-based backends."""
 
-    ...
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _get_filesystem(self, *args, **kwargs):
+        ...
+
+    # core read/writes
+
+    def read_domain(self, *args, **kwargs):
+        return
+
+    def read_domains(self, *args, **kwargs):
+        return
+
+    def write_domain(self, *args, **kwargs):
+        return
+
+    # binary read/writes
+
+    def read_artifact_data(self, *args, **kwargs):
+        return
+
+    def write_artifact_data(self, *args, **kwargs):
+        return
+
+    def read_dataframe_data(self, *args, **kwargs):
+        return
+
+    def write_dataframe_data(self, *args, **kwargs):
+        return
 
 
 class LocalRepository(FsspecRepository):
     """Local filesystem repository leveraging `fsspec`."""
 
-    ...
+    def _get_filesystem(self, *args, **kwargs):
+        return "file"
 
 
 class MemoryRepository(FsspecRepository):
     """In-memory filesystem repository leveraging `fsspec`."""
 
-    ...
+    def _get_filesystem(self, *args, **kwargs):
+        return "memory"
 
 
 class S3Repository(FsspecRepository):
     """S3 filesystem repository leveraging `fsspec`."""
 
-    ...
+    def _get_filesystem(self, *args, **kwargs):
+        return "s3"
 
 
 class LocalRepositoryV2(LocalRepository):
