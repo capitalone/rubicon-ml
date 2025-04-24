b7259a94b9cf877906f4112ab5241b0ef2bb1754
Ryan Soley <ryan.e.soley@gmail.com>
2025-04-24 17:36:25 -0400

    test: change necessary imports

--- tests/unit/client/test_config.py
+++ tests/unit/client/test_config.py
@@ -4,7 +4,11 @@ import pytest
 
 from rubicon_ml.client import Config
 from rubicon_ml.exceptions import RubiconException
-from rubicon_ml.repository import LocalRepository, MemoryRepository, S3Repository
+from rubicon_ml.repository.v2 import (
+    LocalRepositoryV2 as LocalRepository,
+    MemoryRepositoryV2 as MemoryRepository,
+    S3RepositoryV2 as S3Repository,
+)
 
 
 def test_parameters():
