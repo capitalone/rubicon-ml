bda51372f71156d878abc24e256f265aa4a31911
Ryan Soley <ryan.e.soley@gmail.com>
2025-07-11 13:14:28 -0400

    ops: use static versioning (#525)
    * refactor: use static versioning
    * ops: update publish workflow triggers
    * ops: remove `fetch-depth` for `versioneer` from other workflows
    * chore: align header comments
    * chore: remove remaining `versioneer` references
    * fix: put `__version__` back at top
    * ops: publish tag and release on successful PyPI upload
    * ops: add a release PR template
    * ops: update docs workflow permissions
    * chore: clean up docstrings
    * fix: docs workflow permissions

--- rubicon_ml/__init__.py
+++ rubicon_ml/__init__.py
@@ -1,7 +1,4 @@
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+__version__ = "0.11.0"
 
 from rubicon_ml.client import (  # noqa: E402
     Artifact,
