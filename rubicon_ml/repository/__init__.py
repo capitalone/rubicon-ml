49d5d861dd68594a5581a6c8d713ca3a4fd0693a
Ryan Soley <ryan.e.soley@gmail.com>
2026-05-08 13:02:53 -0400

    refactor: remove old `BaseRepository`

--- rubicon_ml/repository/__init__.py
+++ rubicon_ml/repository/__init__.py
@@ -1,5 +1,3 @@
-import warnings
-
 from rubicon_ml.repository.base import RepositoryBase
 from rubicon_ml.repository.composite import CompositeRepository
 from rubicon_ml.repository.fsspec import FsspecRepository
@@ -8,25 +6,7 @@ from rubicon_ml.repository.memory import MemoryRepository
 from rubicon_ml.repository.s3 import S3Repository
 from rubicon_ml.repository.wandb import WandBRepository
 
-
-class BaseRepository(FsspecRepository):
-    """Deprecated. Use ``FsspecRepository`` for filesystem backends
-    or ``RepositoryBase`` for the abstract interface.
-    """
-
-    def __init_subclass__(cls, **kwargs):
-        super().__init_subclass__(**kwargs)
-        warnings.warn(
-            f"{cls.__name__} inherits from `BaseRepository`, which is deprecated. "
-            "Inherit from `FsspecRepository` (for filesystem backends) or "
-            "`RepositoryBase` (for custom backends) instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-
-
 __all__ = [
-    "BaseRepository",
     "CompositeRepository",
     "FsspecRepository",
     "LocalRepository",
