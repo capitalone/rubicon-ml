83f4ad1e53d823e18918447f4202623d5019b112
Kyle Robertson <kyle.wesley@me.com>
2026-06-21 16:12:53 -0600

    wip: the simplest possible persistence implementation
    The core data model is a bunch of domain objects serializable to JSON
    indexed by a path string, some binary blob objects also indexed by a
    path string, and dataframe (also indexed by a path string). So, just
    replicate the first two using two SQLite tables. No real meaningful
    schema, no relationships, none of that stuff.
    
    The BaseRepository class assumes this is all on some filesystem-like
    storage backend supported by fsspec, so we'll need to override the read
    methods and dataframe-related methods as well at some point.

--- tests/unit/repository/test_sqlite_repo.py
+++ tests/unit/repository/test_sqlite_repo.py
@@ -1,15 +1,23 @@
+import json
+import sqlite3
+from datetime import datetime
 import uuid
 from unittest.mock import patch
 
 import pandas as pd
 import pytest
-import s3fs
-import sqlite3
 
 from rubicon_ml import domain
 from rubicon_ml.repository import SqliteRepository
 from rubicon_ml.repository.utils import slugify
 
+def json_decode_datetimes(obj):
+    """
+    Convenience decode to make comparing domain objects possible
+    """
+    if obj.get('_type') == 'datetime':
+        return datetime.fromisoformat(obj['value'])
+    return obj
 
 def test_initialization(tmp_path):
     sqlite_repo = SqliteRepository(root_dir=tmp_path)
@@ -17,27 +25,36 @@ def test_initialization(tmp_path):
     assert isinstance(sqlite_repo.con, sqlite3.Connection)
 
 
-# @patch("s3fs.core.S3FileSystem.open")
-# def test_persist_bytes(mock_open):
-#     bytes_data = b"test data {uuid.uuid4()}"
-#     bytes_path = "s3://bucket/root/path/to/data"
-#
-#     s3_repo = S3Repository(root_dir="s3://bucket/root")
-#     s3_repo._persist_bytes(bytes_data, bytes_path)
-#
-#     mock_open.assert_called_once_with(bytes_path, "wb")
-#
-#
-# @patch("s3fs.core.S3FileSystem.open")
-# def test_persist_domain(mock_open):
-#     project = domain.Project(f"Test Project {uuid.uuid4()}")
-#     project_metadata_path = f"s3://bucket/root/{slugify(project.name)}/metadata.json"
-#
-#     s3_repo = S3Repository(root_dir="s3://bucket/root")
-#     s3_repo._persist_domain(project, project_metadata_path)
-#
-#     mock_open.assert_called_once_with(project_metadata_path, "w")
-#
+def test_persist_bytes(tmp_path):
+    bytes_data = b"test data {uuid.uuid4()}"
+    bytes_path = tmp_path / "some" / "other" / "path"
+
+    repo = SqliteRepository(tmp_path)
+    repo._persist_bytes(bytes_data, bytes_path)
+
+    # Validate we can read the bytes back
+    rows = repo.con.execute(f"SELECT * FROM {repo.BINARY_TABLE}").fetchall()
+    assert len(rows) == 1
+    row = rows.pop()
+    assert row[2] == bytes_data
+
+
+def test_persist_domain(tmp_path):
+    project = domain.Project(f"Test Project {uuid.uuid4()}")
+    project_metadata_path = tmp_path / f"{slugify(project.name)}/metadata.json"
+
+    repo = SqliteRepository(root_dir=tmp_path)
+    repo._persist_domain(project, project_metadata_path)
+
+    # Validate we can read the bytes back
+    rows = repo.con.execute(f"SELECT * FROM {repo.DOMAIN_TABLE}").fetchall()
+    assert len(rows) == 1
+    row = rows.pop()
+    project_metadata = json.loads(row[2], object_hook=json_decode_datetimes)
+    new_project = domain.Project(**project_metadata)
+    assert new_project == project
+
+
 #
 # @patch("s3fs.core.S3FileSystem.open")
 # def test_persist_domain_throws_error(mock_open):
